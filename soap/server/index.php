1f99ea6aa493d6f612dbe0ba32690c8006fc59d8
paolo petrozzi <91311991+ppetrozzi@users.noreply.github.com>
2025-01-06 19:59:38 +0100

    Update index.php

--- soap/server/index.php
+++ soap/server/index.php
@@ -4,34 +4,19 @@
 
 function sayHello($qty, $valuta){
 
-//$val=$_POST['val'];
-//$valuta=$_POST['valu'];
-
 $fileXML="http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-
 $xml = simplexml_load_file($fileXML);
+foreach($xml->Cube[0]->Cube[0]->Cube as $a) {
 
-foreach($xml->Cube[0]->Cube[0]->Cube as $a)
-{
-    
-    switch((string) $a['currency']) { // Get attributes as element indices
-        case $valuta==$a['currency']:
-            $conv=($a['rate'][0])*$qty;
-            break;
-        case $valuta==$a['currency']:
-            $conv=($a['rate'][0])*$qty;
-            break;
-        }
+    if ($valuta==$a['currency']) {
+        $conv=($a['rate'][0])*$qty;
+    }
 }
-   
    return $conv; 
-
 }
 
 $server= new SoapServer("test.wsdl");
-
 $server->addFunction("sayHello");
-
 $server->handle();
 
 ?>
