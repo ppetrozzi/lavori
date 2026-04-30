05ccefb1c2221ceefe7b73702847d99a572acbbb
paolo petrozzi <91311991+ppetrozzi@users.noreply.github.com>
2026-04-30 13:13:43 +0200

    Update leaflet.js

--- geo/leaflet.js
+++ geo/leaflet.js
@@ -4,6 +4,6 @@
  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
   </head>
   <body>
-    <div id="map"></div>
+    <div id="map" style="height: 180px;"></div>
   </body>
 </html>
